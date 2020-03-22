dac380b4589655878366abd4519b4f4fa2ed7f12
victoria <vika.zhak1999@gmail.com>
2020-03-22 14:57:55 +0300

    reformat

--- src/main/java/HttpCodes.java
+++ src/main/java/HttpCodes.java
@@ -1,12 +1,9 @@
 public enum HttpCodes {
     OK(200, "OK"),
     CREATED(201, "Created"),
-    MOVED(302, "Moved Temporarily"),
     BAD_REQUEST(400, "Bad Request"),
-    UNAUTHORIZED(401, "Unauthorized "),
     FORBIDDEN(403, "Forbidden"),
     NOT_FOUND(404, "Not Found"),
-    NOT_ACCEPTABLE(406, "Not Acceptable"),
     SERVER_ERROR(500, "Internal Server Error "),
     NOT_IMPLEMENTED(501, "Not Implemented"),
     SERVICE_UNAVAILABLE(503, "Service Unavailable"),
